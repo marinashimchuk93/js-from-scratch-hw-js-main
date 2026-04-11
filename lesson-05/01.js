63fb6a1dd673dd79c0c4ebc28c92e436d562cac8
Marina Shimchuk <marina.tsigir93@gmail.com>
2026-04-11 13:39:02 +0300

    add lesson-05

---
+++ lesson-05/01.js
@@ -0,0 +1,32 @@
+/*
+Реализовать метод `addResource`, который будет добавлять указанное количество ресурсов к уже существующим ресурсам в объекте `game`.
+
+Требования к методу:
+
+1. Метод должен принимать два параметра:
+  - `resource` (строка) — тип ресурса (`gold`, `lumber` и т.д.).
+  - `amount` (число) — количество ресурса, которое нужно добавить.
+2. Если тип ресурса не существует в объекте `game.resources`, метод должен выводить в консоль сообщение "Invalid resource".
+3. Если ресурс существует, метод должен добавлять значение `amount` к текущему количеству этого ресурса в объекте.
+*/
+
+const game = {
+  resources: {
+    gold: 250,
+    lumber: 100,
+  },
+  addResource(resource, amount) {
+    if (typeof amount !== "number" || amount <= 0) {
+      console.log("Invalid amount");
+      return;
+    }
+    if (resource in this.resources) {
+      this.resources[resource] += amount;
+    } else {
+      console.log("Invalid resource");
+    }
+  },
+};
+
+game.addResource("gold", 50);
+console.log(game.resources);
