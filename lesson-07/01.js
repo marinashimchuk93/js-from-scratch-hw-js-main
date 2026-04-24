315231bd7afb949e3a0af9ae2104784542254425
Marina Shimchuk <marina.tsigir93@gmail.com>
2026-04-24 13:54:03 +0300

    add lesson-07

---
+++ lesson-07/01.js
@@ -0,0 +1,51 @@
+/*
+В этом задании вам предстоит разработать интерактивный слайдер, который позволит пользователю переключаться между созданными нейросетью 
+изображениями веб-технологий.
+Изучите файл index.html. В этом задании надо будет работать с секцией "Урок 6". Разметка уже написано - нужно добавить только js-код.
+
+Задание:
+- Добавьте функциональность кнопкам "prev" и "next", чтобы при их нажатии изображение в теге `<img>` менялось на предыдущее или следующее
+- Обеспечьте циклическое переключение изображений: после последнего изображения следует первое, и наоборот.
+
+Адреса изображений находятся в массиве WEB_TECH_IMAGE
+Первоначально отображается первое изображение из WEB_TECH_IMAGE
+
+🧙‍♂️ Совет: обратите внимание на управление индексом текущего изображения — это ключ к успешному переключению изображений.
+*/
+
+const WEB_TECH_IMAGES = [
+  "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg",
+  "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg",
+  "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg",
+];
+
+const imageElement = document.querySelector("#web-tech-image");
+const buttonElementPrev = document.querySelector("#prev-button");
+const buttonElementNext = document.querySelector("#next-button");
+
+// Переменная хранит номер текущей картинки (начиная с 0)
+let currentIndex = 0;
+
+// Функция для обновления картинки, меняет путь к файлу у картинки на экране.
+function updateImage() {
+  imageElement.src = WEB_TECH_IMAGES[currentIndex];
+}
+
+buttonElementPrev.addEventListener("click", () => {
+  currentIndex++;
+  if (currentIndex >= WEB_TECH_IMAGES.length) {
+    currentIndex = 0; // Переход к первой картинке
+  }
+  updateImage();
+});
+
+buttonElementNext.addEventListener("click", () => {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = WEB_TECH_IMAGES.length - 1; // Переход к последней картинке
+  }
+  updateImage();
+});
+
+// Установка начального изображения
+updateImage();
