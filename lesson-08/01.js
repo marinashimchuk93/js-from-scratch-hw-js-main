29a8de17dbb327cccd77e7f7dd65f89717cbff72
Marina Shimchuk <marina.tsigir93@gmail.com>
2026-05-01 11:24:28 +0300

    add lesson-08

---
+++ lesson-08/01.js
@@ -0,0 +1,16 @@
+/*
+  Нужно написать функцию capitalizeWords, которая принимает строку и делает так, чтобы каждое слово начиналось с заглавной буквы.
+  Например, если передать строку "hello world from javascript", функция должна вернуть "Hello World From JavaScript".
+
+  🧙‍♂️Эту задачу можно решить при помощь цикла for. Рекомендуем реализовать этот вариант решения, как самый эффективный.
+  Другой вариант - использовать метод split строк, и метод массива join.
+*/
+
+function capitalizeWords(string) {
+  return string
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
+console.log(capitalizeWords("hello world from javascript"));
---
+++ lesson-08/02.js
@@ -0,0 +1,15 @@
+/*
+ * Напишите функцию isNumeric, которая принимает строку и проверяет, представляет ли эта строка корректное числовое значение.
+ * Если строка является числом, функция должна возвращать true, в противном случае - false.
+ */
+
+function isNumeric(str) {
+  // your code
+  return !isNaN(parseFloat(str)) && isFinite(str);
+}
+
+console.log(isNumeric("123")); // Ожидаемый результат: true
+console.log(isNumeric("12.3")); // Ожидаемый результат: true
+console.log(isNumeric("123abc")); // Ожидаемый результат: false
+console.log(isNumeric("abc")); // Ожидаемый результат: false
+console.log(isNumeric(" ")); // Ожидаемый результат: false
