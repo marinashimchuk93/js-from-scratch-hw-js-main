d954a71ec9b96fdcf37716036ba9dc2f5fb7f78b
Marina Shimchuk <marina.tsigir93@gmail.com>
2026-05-12 16:24:47 +0300

    add lesson 09

---
+++ lesson-09/01.js
@@ -0,0 +1,84 @@
+/*
+  Изучите файл index.html (секцию "Урок 8"). Разметка уже написана - нужно добавить только js-код.
+
+  Функционал магазина питомцев почти готов. Не хватает возможности добавлять питомцев в корзину.
+  Ваша задача написать обработчик события, который будет добавлять питомцев в корзину.
+
+  1. При клике на кнопку с питомцем, id питомца должен добавляться в массив cart.
+  2. После добавления питомца в корзину, необходимо вызвать функцию updateCartDisplay (она обновит отображение корзины).
+  3. В корзину можно добавить не более 3 питомцев. Если пользователь пытается добавить больше, то в messageBox должен появится текст: 
+  'Вы не можете добавить более 3 питомцев'
+
+  ❕❕❕ Представленный в задании код не следует изменять. Требуется только дописать обработчик события.
+
+  🧙 Подсказка: используй делегирование - будет достаточно одного обработчика событий на контейнере
+  🧙 Подсказка: если пользователь кликнет по кнопке с питомцев, id питомца можно будет получить из объекта события (event.target.id)
+*/
+
+const PETS = [
+  { id: "cat", title: "🐱" },
+  { id: "dog", title: "🐶" },
+  { id: "parrot", title: "🦜" },
+  { id: "fish", title: "🐠" },
+  { id: "spider", title: "🕷" },
+  { id: "snake", title: "🐍" },
+  { id: "hamster", title: "🐹" },
+  { id: "turtle", title: "🐢" },
+  { id: "chinchilla", title: "🦇" },
+  { id: "hedgehog", title: "🦔" },
+  { id: "rat", title: "🐀" },
+  { id: "frog", title: "🐸" },
+];
+
+const cart = [];
+
+const petShop = document.querySelector(".pet-shop");
+const cartList = document.getElementById("cart-list");
+const messageBox = document.getElementById("message-box");
+const clearCartButton = document.getElementById("clear-cart-button");
+
+// Рендерим кнопки для питомцев
+for (let i = 0; i < PETS.length; i++) {
+  const pet = PETS[i];
+
+  const petButtonElement = document.createElement("button");
+  petButtonElement.classList.add("pet");
+  petButtonElement.id = pet.id;
+  petButtonElement.textContent = pet.title;
+
+  petShop.append(petButtonElement);
+}
+
+// Обновляем отображение корзины
+function updateCartDisplay() {
+  cartList.innerHTML = "";
+
+  for (let i = 0; i < cart.length; i++) {
+    const petId = cart[i];
+    const pet = PETS.find((item) => item.id === petId);
+    const petSpanElement = document.createElement("li");
+    petSpanElement.classList.add("pet");
+    petSpanElement.textContent = pet.title;
+    cartList.append(petSpanElement);
+  }
+}
+
+clearCartButton.addEventListener("click", function () {
+  cart.length = 0;
+  updateCartDisplay();
+});
+
+// Твой код:
+petShop.addEventListener("click", function (event) {
+  if (event.target.classList.contains("pet")) {
+    const petId = event.target.id;
+
+    if (cart.length < 3) {
+      cart.push(petId);
+      messageBox.textContent = "";
+      updateCartDisplay();
+    } else {
+      messageBox.textContent = "Вы не можете добавить более 3 питомцев";
+    }
+  }
+});
---
+++ lesson-09/02.js
@@ -0,0 +1,66 @@
+/*
+Задание: Реализация интерактивного отсчёта запуска ракеты
+
+Цель задания: Написать код для обработки событий нажатия на кнопки "Старт" и "Отмена", реализуя обратный отсчёт с помощью JavaScript.
+
+Задача:
+
+1. Добавить обработчик событий для кнопки "Старт":
+
+- При нажатии на кнопку "Старт" должен начаться обратный отсчёт с 3 до 1. Значение счётчика должно отображаться в элементе countdownDisplay. Цифра 3 должна отобразиться немедленно.
+- Каждую секунду значение счётчика должно уменьшаться на 1.
+- Когда счётчик достигнет 0, вместо 0 должен отображаться эмодзи ракеты "🚀" и таймер должен быть остановлен. Пользователь видит: 3 -> 2 -> 1 -> 🚀
+
+
+2. Добавить обработчик событий для кнопки "Отмена":
+
+- При нажатии на кнопку "Отмена" таймер должен быть остановлен
+- В элементе отображения счётчика (countdownDisplay) должно появиться сообщение "Отменено".
+- Отмена таймера возможна только во время его работы
+- После отмены таймера возможен повторный запуск ракеты
+
+Обратите внимание на корректность работы таймера: повторное нажатие на "Старт" не должно приводить к нескольким одновременно работающим таймерам.
+
+Подсказки:
+- 🧙‍♂️ Для выполнения этого задания нужно познакомиться с браузерными функциями setInterval (https://doka.guide/js/setinterval/) и clearInterval(https://doka.guide/js/clearinterval/). Они очень похоже на setTimeout и clearTimeout.
+ */
+
+const startButton = document.getElementById("start");
+const cancelButton = document.getElementById("cancel");
+const countdownDisplay = document.getElementById("countdown");
+
+let isTimerStarted = false;
+let timerId;
+
+startButton.addEventListener("click", () => {
+  let counter = 3;
+
+  // your code
+
+  if (isTimerStarted) return;
+  isTimerStarted = true;
+
+  countdownDisplay.textContent = counter;
+
+  timerId = setInterval(() => {
+    counter--;
+
+    if (counter > 0) {
+      countdownDisplay.textContent = counter;
+    } else {
+      countdownDisplay.textContent = "🚀";
+      clearInterval(timerId);
+      isTimerStarted = false;
+    }
+  }, 1000);
+});
+
+cancelButton.addEventListener("click", () => {
+  // your code
+
+  if (isTimerStarted) {
+    clearInterval(timerId);
+    isTimerStarted = false;
+    countdownDisplay.textContent = "Отменено";
+  }
+});
